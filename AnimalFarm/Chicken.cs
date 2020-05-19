c670b27a080191459f9d2027b1407f2e6e337930
synset <synnove.settman@yahoo.com>
2020-05-19 11:43:28 +0200

    Add project files.

---
+++ AnimalFarm/Chicken.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalFarm
+{
+    class Chicken
+    {
+        private string name;
+        private int age;
+        private int productPerDay;
+        private const int minAge = 0;
+        private const int maxAge = 15;
+
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null, empty or whitespace.");
+                }
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if(value < minAge || value > maxAge)
+                {
+                    throw new ArgumentException("Age must be between 0 and 15.");
+                }
+                this.age = value;
+            }
+        }
+
+        public int ProductPerDay
+        {
+            get 
+            {
+                return this.CalculateProductPerDay(age);
+            }
+        }
+
+        private int CalculateProductPerDay(int age)
+        {
+            if (age >= 0 && age < 5)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ AnimalFarm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AnimalFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Chicken> chickens = new List<Chicken>()
+            {
+                new Chicken("Henny", 3),
+                new Chicken("Hen", 8),
+                new Chicken("May", 10),
+                new Chicken("Minni", 2)
+            };
+
+            foreach (Chicken chicken in chickens)
+            {
+                Console.WriteLine("Chicken " + chicken.Name + " (age " + chicken.Age + ") can produce " + chicken.ProductPerDay + " eggs per day.");
+            }
+        }
+    }
+}
